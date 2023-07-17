13325553200580bbce9607beb3ff3ae1f772ff4f
Orhun <78608829+Orhns@users.noreply.github.com>
2023-07-17 11:53:31 +0300

    Applying EF

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,18 +1,32 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 
 namespace ConsoleUI
 {
+    //SOLID
+    //open closed principle
     internal class Program
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var item in productManager.GetAll())
+            //foreach (var item in productManager.GetAll())
+            //{
+            //    Console.WriteLine(item.ProductName);
+            //}
+
+            //foreach (var item in productManager.GetAllByCategoryId(2))
+            //{
+            //    Console.WriteLine(item.ProductName);
+            //}
+            
+            foreach (var item in productManager.GetByUnitPrice(50,100))
             {
                 Console.WriteLine(item.ProductName);
             }
+
         }
     }
 }
