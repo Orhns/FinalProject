9b8d94e20928da6fe71bccb78df1f02ab306e608
Orhun <78608829+Orhns@users.noreply.github.com>
2023-07-29 01:27:15 +0300

    Adding core layer

--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -18,5 +18,18 @@ namespace DataAccess.Concrete.EntityFramework
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Personel> Personels { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //fluent mapping
+            //modelBuilder.HasDefaultSchema("admin");
+            modelBuilder.Entity<Personel>().ToTable("Employees");
+
+            modelBuilder.Entity<Personel>().Property(p=>p.Id).HasColumnName("EmployeeID");
+            modelBuilder.Entity<Personel>().Property(p=>p.Name).HasColumnName("FirstName");
+            modelBuilder.Entity<Personel>().Property(p=>p.Surname).HasColumnName("LastName");
+        }
     }
 }
